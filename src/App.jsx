8d014b660a0db2f6f7d7d01b78d9a24fd749fd6e
Youkwhan <Youkwhan@gmail.com>
2023-07-01 18:54:55 -0400

    Start: About page, Add: name section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.scss"
 import Header from "./components/Header/Header"
-import Home from "./components/Home/Home"
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<main>
-				<div className="page" id="page">
+				<div className="page" id="home">
 					<Home />
+				</div>
+				<div className="page" id="about">
+					<About />
 				</div>
 			</main>
 		</div>
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,23 @@
+import "./About.scss"
+
+function About() {
+	return (
+		<div className="about">
+			<div className="name">
+				<div>
+					<h1 className="txt">
+						AB <br />
+						OU
+					</h1>
+				</div>
+				<h1 className="txt lg">T</h1>
+
+				<div className="me">ME</div>
+			</div>
+
+			
+		</div>
+	)
+}
+
+export default About
