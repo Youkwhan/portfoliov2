d64972f9b0704825d0f7aa05ef72dac086645ba4
Youkwhan <Youkwhan@gmail.com>
2023-07-01 19:07:35 -0400

    Add: InfoCards

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import "./About.scss"
+import InfoCard from "../../components/InfoCard/InfoCard"
 
 function About() {
 	return (
@@ -15,7 +16,35 @@ function About() {
 				<div className="me">ME</div>
 			</div>
 
-			
+			<div className="info">
+				<InfoCard
+					title="About Me"
+					details="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+				debitis, soluta voluptatem architecto nesciunt odio consequuntur eos, ad
+				neque veniam modi blanditiis nobis rem obcaecati tenetur cum impedit
+				accusamus accusantium officia earum vel quod suscipit. Velit, quibusdam.
+				Velit laboriosam ducimus nostrum sequi vel veniam eligendi soluta
+				exercitationem? Recusandae, ipsa numquam."
+				/>
+            <InfoCard
+					title="Education"
+					details="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+				debitis, soluta voluptatem architecto nesciunt odio consequuntur eos, ad
+				neque veniam modi blanditiis nobis rem obcaecati tenetur cum impedit
+				accusamus accusantium officia earum vel quod suscipit. Velit, quibusdam.
+				Velit laboriosam ducimus nostrum sequi vel veniam eligendi soluta
+				exercitationem? Recusandae, ipsa numquam."
+				/>
+            <InfoCard
+					title="Skills"
+					details="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+				debitis, soluta voluptatem architecto nesciunt odio consequuntur eos, ad
+				neque veniam modi blanditiis nobis rem obcaecati tenetur cum impedit
+				accusamus accusantium officia earum vel quod suscipit. Velit, quibusdam.
+				Velit laboriosam ducimus nostrum sequi vel veniam eligendi soluta
+				exercitationem? Recusandae, ipsa numquam."
+				/>
+			</div>
 		</div>
 	)
 }
