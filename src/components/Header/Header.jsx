df73db91fb3976b1199724aee92826c29371d9ba
Youkwhan <Youkwhan@gmail.com>
2023-07-01 13:44:04 -0400

    Add: indicator, which is the active navLink
    - Select each of the list item in the navigation menu and also the indicator
    - Compute the width of the list items and use it to set the width of the indicator (the active link)
    - Which needs to occur after the component has mounted, thus useEffect hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
 import "./Header.scss"
 import logo from "../../assets/logo.png"
 import profile from "../../assets/profile.jpg"
+import { useEffect } from "react"
 
 function Header() {
+	useEffect(() => {
+		const navLinks = document.querySelectorAll("nav ul li")
+		const indicator = document.querySelector("nav .indicator")
+
+		function update() {
+			let width = navLinks[0].offsetWidth,
+				left = navLinks[0].offsetLeft
+
+			indicator.style.width = `${width + 28}px`
+			indicator.style.left = `${left}px`
+		}
+
+		update()
+	}, [])
+
 	return (
 		<header>
 			<div className="logo">
